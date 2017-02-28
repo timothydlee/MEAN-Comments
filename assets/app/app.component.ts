b72387a5e3ac0acc9f191472e9667eea03585222
Timothy Lee <timothy.d.lee@live.com>
2017-02-28 03:25:30 -0600

    added models, schemas, connected app.component.ts to app.component.html correctly using string interpolation

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -5,5 +5,8 @@ import { Component } from '@angular/core';
     templateUrl: './app.component.html'
 })
 export class AppComponent {
-    
+    message = {
+        content: 'content1',
+        author: 'Tim'
+    };
 }
--- routes/app.js
+++ routes/app.js
@@ -1,17 +1,8 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 router.get('/', function (req, res, next) {
 		res.render('index');
 });
-
-router.get('/message/:msg', function(req, res, next) {
-	res.render('node', {message: req.params.msg});
-})
-
-router.post('/message', function(req, res, next) {
-	let message = req.body.message;
-	res.redirect(`/message/${message}`)
-})
 
 module.exports = router;
