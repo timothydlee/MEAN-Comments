69de5af43b8bc4330c0b46129e11c3608f55b95d
Timothy Lee <timothy.d.lee@live.com>
2017-03-01 16:00:26 -0600

    added signup, logout and signin components and html files, added to app module - not to the authentication app bc of subroutes

---
+++ assets/app/auth/signin.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-signin',
+    template: './signin.component.html'
+})
+
+export class SigninComponent {
+
+}
---
+++ assets/app/auth/signup.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-signup',
+    template: './signup.component.html'
+})
+
+export class SignupComponent {
+
+}
