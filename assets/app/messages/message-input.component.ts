2f2af4fde4798bec20cdb750a4232348b7531cad
Timothy Lee <timothy.d.lee@live.com>
2017-03-01 14:36:05 -0600

    added ngform to manage the form input - resets to blank field and doesn't accept empty input fields

--- assets/app/messages/message-input.component.ts
+++ assets/app/messages/message-input.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { MessageService } from "./message.service";
 import { Message } from "./message.model";
+import { NgForm } from "@angular/forms";
 
 @Component({
     selector: 'app-message-input',
@@ -10,8 +11,9 @@ import { Message } from "./message.model";
 export class MessageInputComponent {
     constructor(private messageService: MessageService){}
 
-    onSave(value: string) {
-        const message = new Message(value, 'Tim');
+    onSubmit(form: NgForm) {
+        const message = new Message(form.value.content, 'Tim');
         this.messageService.addMessage(message);
+        form.resetForm();
     }
 }
