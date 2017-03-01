dcdbefcd7bdd6def6df6ef103bbf013b732e2a9b
Timothy Lee <timothy.d.lee@live.com>
2017-03-01 04:17:31 -0600

    refactored message list to its own component

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -1,14 +1,10 @@
 import { Component } from '@angular/core';
-import { Message } from "./messages/message.model";
+
 
 @Component({
     selector: 'my-app',
     templateUrl: './app.component.html'
 })
 export class AppComponent {
-    messages: Message[] = [
-        new Message('Some message', 'Tim'),
-        new Message('Second message', 'Tim'),
-        new Message('Something else', 'Tim')
-    ];
+
 }
