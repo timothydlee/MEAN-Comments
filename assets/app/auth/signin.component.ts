90ccdd3ca5a51fd85dab4f52ef27c2ae3b4c4b42
Timothy Lee <timothy.d.lee@live.com>
2017-03-02 01:37:03 -0600

    leveraged signup component and html to create similar signin component and html

--- assets/app/auth/signin.component.ts
+++ assets/app/auth/signin.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
     selector: 'app-signin',
@@ -6,5 +7,20 @@ import { Component } from "@angular/core";
 })
 
 export class SigninComponent {
+    myForm: FormGroup;
 
+    onSubmit() {
+        console.log(this.myForm);
+        this.myForm.reset();
+    }
+
+    ngOnInit() {
+        this.myForm = new FormGroup({
+            email: new FormControl(null, [
+                Validators.required,
+                Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
+            ]),
+            password: new FormControl(null, Validators.required),
+        });
+    }
 }
