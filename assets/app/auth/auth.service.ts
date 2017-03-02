0b3a43d7b545e5c99974a57c6cfc3d7f7a6b5fc3
Timothy Lee <timothy.d.lee@live.com>
2017-03-02 15:16:53 -0600

    added jwt to localstorage and allowed for jwt signin

--- assets/app/auth/auth.service.ts
+++ assets/app/auth/auth.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from "@angular/core";
 import { Http, Headers, Response } from "@angular/http";
 import 'rxjs/Rx';
-import { Observable } from "rxjs";
 
+import { Observable } from "rxjs";
 import { User } from "./user.model";
 
 @Injectable()
@@ -14,6 +14,14 @@ export class AuthService {
         const body = JSON.stringify(user);
         const headers = new Headers({'Content-Type': 'application/json'});
         return this.http.post('http://localhost:3000/user', body, {headers: headers})
+            .map((response: Response) => response.json())
+            .catch((err: Response) => Observable.throw(err.json()));
+    }
+
+    signin(user: User) {
+        const body = JSON.stringify(user);
+        const headers = new Headers({'Content-Type': 'application/json'});
+        return this.http.post('http://localhost:3000/user/signin', body, {headers: headers})
             .map((response: Response) => response.json())
             .catch((err: Response) => Observable.throw(err.json()));
     }
--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
 
 const User = require('../models/user');
 
-router.post('/', function (req, res, next) {
+router.post('/', function(req, res, next) {
 	var user = new User({
 		firstName: req.body.firstName,
 		lastName: req.body.lastName,
@@ -21,6 +22,35 @@ router.post('/', function (req, res, next) {
 		res.status(201).json({
 			message: 'User created',
 			obj: result
+		});
+	});
+});
+
+router.post('/signin', function(req, res, next) {
+	User.findOne({email: req.body.email}, function(err, user) {
+		if (err) {
+			return res.status(500).json({
+				title: 'An error occurred in the user signin route',
+				error: err
+			});
+		}
+		if (!user) {
+			return res.status(401).json({
+				title: 'Login failed',
+				error: {message: 'Invalid login credentials'}
+			});
+		}
+		if (!bcrypt.compareSync(req.body.password, user.password)) {
+			return res.status(401).json({
+				title: 'Login failed',
+				error: {message: 'Invalid login credentials'}
+			});
+		}
+		var token = jwt.sign({user: user}, 'secret', {expiresIn: 7200});
+		res.status(200).json({
+			message: 'Successfully logged in',
+			token: token,
+			userId: user._id
 		});
 	});
 });
