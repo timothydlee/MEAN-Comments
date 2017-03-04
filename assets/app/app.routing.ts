6144248cde2a067178ce845dd01e108055cb3be7
Timothy Lee <timothy.d.lee@live.com>
2017-03-04 17:51:41 -0600

    refactored auth components into a separtae auth module and then refactored lazy loading to only load the auth module when it's actually needed

--- assets/app/app.routing.ts
+++ assets/app/app.routing.ts
@@ -1,13 +1,12 @@
 import { Routes, RouterModule } from "@angular/router";
 import { MessagesComponent } from "./messages/messages.component";
 import { AuthenticationComponent } from "./auth/authentication.component";
-import { AUTH_ROUTES } from "./auth/auth.routes";
 
 const APP_ROUTES: Routes = [
     //PathMatch = only redirect if '' is the entire pathway (in addition to domain)
     { path: '', redirectTo: '/messages', pathMatch: 'full' },
     { path: 'messages', component: MessagesComponent },
-    { path: 'auth', component: AuthenticationComponent, children: AUTH_ROUTES }
+    { path: 'auth', component: AuthenticationComponent, loadChildren: './auth/auth.module#AuthModule' }
 ];
 
 export const routing = RouterModule.forRoot(APP_ROUTES);
