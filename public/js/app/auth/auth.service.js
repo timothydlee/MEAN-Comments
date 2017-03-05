8d32aeac8f99441bb38766994567ef4d0107e4a7
Timothy Lee <timothy.d.lee@live.com>
2017-03-04 19:14:15 -0600

    rebuilt bundle

--- public/js/app/auth/auth.service.js
+++ public/js/app/auth/auth.service.js
@@ -23,7 +23,7 @@ export var AuthService = (function () {
         var _this = this;
         var body = JSON.stringify(user);
         var headers = new Headers({ 'Content-Type': 'application/json' });
-        return this.http.post('https://mean-comments.herokuapp.com/user/signin', body, { headers: headers })
+        return this.http.post('https://mean-comments.herokuapp.com/signin', body, { headers: headers })
             .map(function (response) { return response.json(); })
             .catch(function (error) {
             _this.errorService.handleError(error.json());
