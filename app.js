96902422314a6e747390d68de2886e91ffa34d4d
Timothy Lee <timothy.d.lee@live.com>
2017-03-04 19:43:55 -0600

    changed instances of localhost:3000 to https://mean-comments...

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var messageRoutes = require('./routes/messages');
 var userRoutes = require('./routes/user');
 
 var app = express();
-mongoose.connect('localhost:27017/node-angular');
+mongoose.connect('mongodb://heroku_1q948fl4:hlesqdsmkgla4im421vdvkbjgm@ds117830.mlab.com:17830/heroku_1q948fl4');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- public/js/app/auth/auth.service.js
+++ public/js/app/auth/auth.service.js
@@ -12,7 +12,7 @@ export var AuthService = (function () {
         var _this = this;
         var body = JSON.stringify(user);
         var headers = new Headers({ 'Content-Type': 'application/json' });
-        return this.http.post('http://localhost:3000/user', body, { headers: headers })
+        return this.http.post('https://mean-comments.herokuapp.com/user', body, { headers: headers })
             .map(function (response) { return response.json(); })
             .catch(function (error) {
             _this.errorService.handleError(error.json());
@@ -23,7 +23,7 @@ export var AuthService = (function () {
         var _this = this;
         var body = JSON.stringify(user);
         var headers = new Headers({ 'Content-Type': 'application/json' });
-        return this.http.post('http://localhost:3000/user/signin', body, { headers: headers })
+        return this.http.post('https://mean-comments.herokuapp.com/user/signin', body, { headers: headers })
             .map(function (response) { return response.json(); })
             .catch(function (error) {
             _this.errorService.handleError(error.json());
